5f849a0acb37e594ea00517638ce93d92d695e81
camille-004 <dunningcamille@gmail.com>
2021-02-19 22:12:29 -0800

    First AI commit

--- backend/modules/README.md
+++
@@ -1,1 +0,0 @@
-make a folder for your /api route in here
---
+++ backend/modules/image_rec/text_data.py
@@ -0,0 +1,51 @@
+import io
+import os
+
+import pandas as pd
+from google.cloud import vision
+from google.cloud.vision_v1 import types
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'ServiceAccountToken.json'
+
+client = vision.ImageAnnotatorClient()
+
+FILE_NAME = 'demo1.jpeg'
+FOLDER_PATH = 'text_images'
+
+with io.open(os.path.join(FOLDER_PATH, FILE_NAME), 'rb') as image_file:
+    content = image_file.read()
+
+image = types.Image(content=content)
+
+response = client.text_detection(image=image)
+paras = response.full_text_annotation.pages[
+    0].blocks[
+    0].paragraphs
+
+char_list = []
+bound_poly = []
+
+for p in paras:
+    for w in p.words:
+        for c in w.symbols:
+            if 'detected_break' in c.property and \
+                    c.property.detected_break.type_.name == 'SPACE':
+                char_list.append(c.text)
+                char_list.append('SP')
+                bound_poly.append(c.bounding_box)
+                bound_poly.append(c.bounding_box)
+            else:
+                char_list.append(c.text)
+                bound_poly.append(c.bounding_box)
+
+
+df = pd.DataFrame({'char': char_list, 'bound_poly': bound_poly})
+# for t in text:
+#     df = df.append(
+#         dict(
+#             char=char_list,
+#             bounding_poly=bound_poly
+#         ),
+#         ignore_index=True
+#     )
+
