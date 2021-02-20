85be5d278be48a10c355d5a9ac6db9fe0a01de26
camille-004 <dunningcamille@gmail.com>
2021-02-20 14:45:18 -0800

    generated text bounding dataset

--- backend/modules/image_rec/text_loc_data.py
+++ backend/modules/image_rec/text_loc_data.py
@@ -15,7 +15,6 @@ FOLDER_PATH = 'text_images'
 with io.open(os.path.join(FOLDER_PATH, FILE_NAME), 'rb') as image_file:
     content = image_file.read()
 
-
 image = types.Image(content=content)
 
 response = client.text_detection(image=image)
@@ -39,13 +38,50 @@ for p in paras:
                 char_list.append(c.text)
                 bound_poly.append(c.bounding_box)
 
-
 df = pd.DataFrame({'char': char_list, 'bound_poly': bound_poly})
-# for t in text:
-#     df = df.append(
-#         dict(
-#             char=char_list,
-#             bounding_poly=bound_poly
-#         ),
-#         ignore_index=True
-#     )
+
+
+def split_str(text):
+    str_list = text.split('\n')
+    x_points = []
+    y_points = []
+    for s in str_list:
+        if 'x: ' in s:
+            x_points.append(s.strip())
+        elif 'y: ' in s:
+            y_points.append(s.strip())
+
+    x_dict = {}
+    y_dict = {}
+    for i, x in enumerate(x_points):
+        x_dict['x_' + str(i + 1)] = [
+            int(s) for s in x.split() if s.isdigit()
+        ][0]
+
+    for i, y in enumerate(y_points):
+        y_dict['y_' + str(i + 1)] = [
+            int(s) for s in y.split() if s.isdigit()
+        ][0]
+
+    x_dict.update(y_dict)
+    return x_dict
+
+
+def merge_dicts(df):
+    pos_dicts = []
+    for idx, row in df['bound_poly'].iteritems():
+        pos_dicts.append(split_str(str(row)))
+
+    d_keys = list(pos_dicts[0].keys())
+    final_dict = {k: [] for k in d_keys}
+
+    for d in pos_dicts:
+        for k in d_keys:
+            final_dict[k].append(d[k])
+
+    return final_dict
+
+
+merged_dicts = merge_dicts(df)
+merged_dicts_df = pd.DataFrame(merged_dicts)
+df = pd.concat([df.char, merged_dicts_df], axis=1)
