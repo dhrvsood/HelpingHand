a6e08af0e125f3271eab3ccd9af449828cfc7155
Dhruv Sood <53026542+dhrvsood@users.noreply.github.com>
2021-02-21 16:37:00 +0530

    graphing

--- frontend/src/pages/Insight.jsx
+++ frontend/src/pages/Insight.jsx
@@ -1,4 +1,5 @@
 import React, { useRef, useState, useContext } from "react";
+import Plot from 'react-plotly.js';
 import "./style.less";
 
 // components
@@ -6,13 +7,29 @@ import InsightContext from "../contexts/InsightContext";
 
 const Insight = () => {
   const [insights] = useContext(InsightContext);
+
   return (
+               
     <div className="container">
+        <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
         <p className="header">Insights</p>
         <pre>
           {JSON.stringify(insights)}
         </pre>
         <img src={insights.image}/>
+        <Plot
+        data={[
+          {
+            x: [1, 2, 3],
+            y: [2, 6, 3],
+            type: 'scatter',
+            mode: 'lines+markers',
+            marker: {color: 'red'},
+          },
+          {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+        ]}
+        layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
+      />
     </div>
   );
 };
