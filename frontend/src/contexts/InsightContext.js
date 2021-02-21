c00dd51a436338a0d60f066141561d41b28f0e75
Ronak Shah <ronakshahultimate@gmail.com>
2021-02-20 18:47:38 -0800

    one more thing

--- frontend/src/contexts/InsightContext.js
+++ frontend/src/contexts/InsightContext.js
@@ -1,7 +1,10 @@
 import React from "react";
 
 export const defaultValue = {
-	responseData: {},
+	responseData: {
+		"handwritingScore" : 90,
+		"rating" : "not bad, work on dotting your is"
+	},
 	image: "https://via.placeholder.com/200"
 };
 export default React.createContext([defaultValue, () => {}]);
